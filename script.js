acc368e11a259d7780e1da9ae3d7e5f6fcf22085
Best-TechSavvy <techsavvy1st@gmail.com>
2025-11-28 11:38:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var sectall = ["secthome", "section1", "section2", "section3", "section4"];
 var btnall = ["btnhome", "btn1", "btn2", "btn3", "btn4"];
 var currentSection = "secthome";
+var width = window.innerWidth;
 
 function view(sect, btn) {
     if (sect === currentSection) return;
@@ -24,7 +25,21 @@ function view(sect, btn) {
         let button = document.getElementById(btnall[i]);
         button.classList.toggle("active", btn === btnall[i]);
     }
+
+    if (width < 768) {
+        document.getElementById("navbar").style.display = "none";
+        document.getElementById("more").style.display = "block";
+    }
 }
+
+//phone object bellow
+function more(){
+    var more = document.getElementById("more");
+    var navbar = document.getElementById("navbar");
+    navbar.style.display = "block";
+    more.style.display = "none";
+}
+//phone object above
 
 window.onload = function () {
     let home = document.getElementById("secthome");
@@ -32,4 +47,25 @@ window.onload = function () {
     home.style.animation = "slideIn 0.5s ease forwards";
     currentSection = "secthome";
     document.getElementById("btnhome").classList.add("active");
+    
+    var phone = ["more"];
+    var computer = ["navbar"];
+
+    if (width < 768) {
+        for (let i = 0; i < phone.length; i++) {
+            document.getElementById(phone[i]).style.display = "block";
+        }
+        for (let i = 0; i < computer.length; i++) {
+            document.getElementById(computer[i]).style.display = "none";
+        }
+        document.getElementById("main").style.marginLeft = "0px";
+    } else {
+        for (let i = 0; i < phone.length; i++) {
+            document.getElementById(phone[i]).style.display = "none";
+        }
+        for (let i = 0; i < computer.length; i++) {
+            document.getElementById(computer[i]).style.display = "block";
+        }
+        document.getElementById("main").style.marginLeft = "200px";
+    }
 };
